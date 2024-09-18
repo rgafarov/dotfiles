2a4a87531973e75bd1a016a887a9a524c5c17779
Rustem Gafarov <grm2001@mail.ru>
2024-09-18 14:08:58 +0300

    added tmux plugin for saving profile, added lsp for python

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,16 +1,18 @@
 require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'navarasu/onedark.nvim'
-	use {'nmac427/guess-indent.nvim', config = function() require('guess-indent').setup {} end, }
-	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use { 'nmac427/guess-indent.nvim', config = function() require('guess-indent').setup {} end, }
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 	use 'nvim-treesitter/nvim-treesitter-context'
-	use {'crispgm/nvim-tabline', config = function() require('tabline').setup({}) end, }
+	use { 'crispgm/nvim-tabline', config = function() require('tabline').setup({}) end, }
 	use 'neovim/nvim-lspconfig'
+	use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { { 'nvim-lua/plenary.nvim' } } }
+	use 'lewis6991/gitsigns.nvim'
 end)
 
 require('onedark').load()
 
-require('onedark').setup  {
+require('onedark').setup {
 	-- Main options --
 	style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
 }
@@ -25,15 +27,24 @@ require 'nvim-treesitter.configs'.setup {
 	},
 }
 
-require 'treesitter-context'.setup{}
+require 'treesitter-context'.setup {}
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "TelescopePreviewerLoaded",
+	callback = function(args)
+		vim.wo.number = true
+	end,
+})
+
+require('gitsigns').setup {}
 
 local lspconfig = require('lspconfig')
 
-lspconfig.lua_ls.setup{}
+lspconfig.lua_ls.setup {}
 
-lspconfig.clangd.setup{}
+lspconfig.clangd.setup {}
 
-lspconfig.gopls.setup{
+lspconfig.gopls.setup {
 	cmd = { 'gopls' },
 	capabilities = capabilities,
 	settings = {
@@ -47,50 +58,59 @@ lspconfig.gopls.setup{
 	},
 }
 
+lspconfig.pylsp.setup {}
 
 -- Mappings.
+vim.g.mapleader = " "
+
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+--
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
 	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-	
+
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wl', function()
+	vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<leader>wl', function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, bufopts)
-	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'lua_ls', 'clangd' , 'gopls' }
+local servers = { 'lua_ls', 'clangd', 'gopls', 'pylsp' }
 for _, lsp in pairs(servers) do
 	require('lspconfig')[lsp].setup {
 		on_attach = on_attach,
-			flags = {
-				-- This will be the default in neovim 0.7+
-				debounce_text_changes = 150,
-			}
+		flags = {
+			-- This will be the default in neovim 0.7+
+			debounce_text_changes = 150,
 		}
+	}
 end
